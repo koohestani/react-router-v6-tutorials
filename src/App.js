e99bc5fcdc2a4347351ee81dd2c2b6ef842d72eb
hamed koohestani <hamedk9776@gmail.com>
2021-12-25 08:57:17 +0330

    comment for search params

--- src/App.js
+++ src/App.js
@@ -1,75 +1,77 @@
 import React from 'react';
 import {
-  Route,
-  Routes,
-  Link,
-  NavLink,
-  Outlet,
-  useParams,
-  Navigate,
-  useLocation,
-  useNavigate,
-  useSearchParams,
-  useRoutes,
+    Route,
+    Routes,
+    Link,
+    NavLink,
+    Outlet,
+    useParams,
+    Navigate,
+    useLocation,
+    useNavigate,
+    useSearchParams,
+    useRoutes,
 } from 'react-router-dom';
 
 const App = () => (
-  <Routes>
-    <Route path='/' element={<Layout />}>
-      <Route index element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path='contact' element={<Contact />} />
-      {/* <Route path='products' element={<Products />} />
+    <Routes>
+        <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='about' element={<About />} />
+            <Route path='contact' element={<Contact />} />
+            {/* <Route path='products' element={<Products />} />
           <Route path='products/:productId' element={<ProductItem />} /> */}
-      <Route element={<RequireAuth />}>
-        <Route path='products' element={<ProductLayout />}>
-          <Route index element={<Products />} />
-          <Route path=':productId' element={<ProductItem />} />
+            <Route element={<RequireAuth />}>
+                <Route path='products' element={<ProductLayout />}>
+                    <Route index element={<Products />} />
+                    <Route path=':productId' element={<ProductItem />} />
+                </Route>
+            </Route>
         </Route>
-      </Route>
-    </Route>
-    <Route path='/login' element={<Login />} />
-    <Route path='*' element={<NotFound />} />
-  </Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='*' element={<NotFound />} />
+    </Routes>
 );
 
 const ProductLayout = () => {
-  return <Outlet />;
+    return <Outlet />;
 };
 
 const Products = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const brand = searchParams.get('brand');
-  // const brands = searchParams.getAll('brand');
+    const [searchParams, setSearchParams] = useSearchParams();
+    const brand = searchParams.get('brand');
+    // const brands = searchParams.getAll('brand');
 
-  const handleClick = () => {
-    searchParams.set('negar', 'hamed');
-    searchParams.delete('brand');
-    // setSearchParams(searchParams, {
-    //   replace: true,
-    // });
-    setSearchParams(
-      {
-        salam: 'ssss',
-      },
-      {
-        replace: true,
-      }
+    const handleClick = () => {
+        // work with search params with search params hook
+        // searchParams.set('count', 20);
+        // searchParams.delete('brand');
+        // setSearchParams(searchParams, {
+        //   replace: true,
+        // });
+        // update search params with object
+        setSearchParams(
+            {
+                brand: 'apple',
+            },
+            {
+                replace: true,
+            }
+        );
+    };
+
+    return (
+        <div>
+            {brand ? <h2>Your selected brand is {brand}</h2> : ''}
+            <h1>This is a shop page!</h1>
+            <button onClick={handleClick}>add brand salam</button>
+        </div>
     );
-  };
-
-  return (
-    <div>
-      {brand ? <h2>Your selected brand is {brand}</h2> : ''}
-      <h1>This is a shop page!</h1>
-      <button onClick={handleClick}>add brand salam</button>
-    </div>
-  );
 };
 
 const ProductItem = () => {
-  const { productId } = useParams();
-  return <h1>This is product item {productId}</h1>;
+    const { productId } = useParams();
+    return <h1>This is product item {productId}</h1>;
 };
 
 const Home = () => <h1>Home Page</h1>;
@@ -79,83 +81,83 @@ const About = () => <h1>About Page</h1>;
 const Contact = () => <h1>Contact Page</h1>;
 
 const Header = () => (
-  <div>
-    <ul>
-      <li>
-        <ActiveStyleNavLink to='/' label='Home' />
-      </li>
-      <li>
-        <ActiveStyleNavLink to='/about' label='About' />
-      </li>
-      <li>
-        <ActiveStyleNavLink to='/contact' label='Contact' />
-      </li>
-    </ul>
-  </div>
+    <div>
+        <ul>
+            <li>
+                <ActiveStyleNavLink to='/' label='Home' />
+            </li>
+            <li>
+                <ActiveStyleNavLink to='/about' label='About' />
+            </li>
+            <li>
+                <ActiveStyleNavLink to='/contact' label='Contact' />
+            </li>
+        </ul>
+    </div>
 );
 
 const Footer = () => <p>This is a footer !</p>;
 
 const Layout = () => (
-  <>
-    <Header />
-    <Outlet />
-    <Footer />
-  </>
+    <>
+        <Header />
+        <Outlet />
+        <Footer />
+    </>
 );
 
 const NotFound = () => (
-  <div>
-    <h1>nothing here</h1>
-    <Link to='/'>go to home page</Link>
-  </div>
+    <div>
+        <h1>nothing here</h1>
+        <Link to='/'>go to home page</Link>
+    </div>
 );
 
 const RequireAuth = () => {
-  const { pathname } = useLocation();
-  const auth = true;
-  return auth ? (
-    <Outlet />
-  ) : (
-    <Navigate to='/login' state={{ from: pathname }} />
-  );
+    const { pathname } = useLocation();
+    const auth = true;
+    return auth ? (
+        <Outlet />
+    ) : (
+        <Navigate to='/login' state={{ from: pathname }} />
+    );
 };
 
 const Login = () => {
-  const { state } = useLocation();
-  const navigate = useNavigate();
-  const from = state ? (state.from ? state.from : '/') : '/';
+    const { state } = useLocation();
+    const navigate = useNavigate();
+    const from = state ? (state.from ? state.from : '/') : '/';
 
-  const handleClick = () => {
-    navigate(from, {
-      replace: true,
-    });
-  };
+    const handleClick = () => {
+        navigate(from, {
+            replace: true,
+        });
+    };
 
-  return (
-    <div>
-      <h1>Welcome from {from}</h1>
-      <h2>This is login page</h2>
-      <button onClick={handleClick}>Login</button>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Welcome from {from}</h1>
+            <h2>This is login page</h2>
+            <button onClick={handleClick}>Login</button>
+        </div>
+    );
 };
 
 const ActiveStyleNavLink = ({ to, label }) => {
-  return (
-    <NavLink
-      to={to}
-      style={({ isActive }) =>
-        isActive
-          ? {
-              backgroundColor: 'yellow',
+    return (
+        <NavLink
+            to={to}
+            style={({ isActive }) =>
+                isActive
+                    ? {
+                          backgroundColor: 'yellow',
+                      }
+                    : undefined
             }
-          : undefined
-      }
-    >
-      {label}
-    </NavLink>
-  );
+        >
+            {label}
+        </NavLink>
+    );
 };
 
 // const ROUTES = [
